aa5a8dd1959ae40813e7addd74d852c6e52f8bcc
andreibel <andreinw12@gmail.com>
2025-11-30 22:42:34 +0200

    rename server classes and update Serve class to extend AbstractServer with message handling

--- server/src/main/java/com/andreibel/server/controller/Serve.java
+++ server/src/main/java/com/andreibel/server/controller/Serve.java
@@ -1,4 +1,20 @@
 package com.andreibel.server.controller;
 
-public class Serve {
+import ocsf.server.AbstractServer;
+import ocsf.server.ConnectionToClient;
+
+public class Serve extends AbstractServer {
+    /**
+     * Constructs a new server.
+     *
+     * @param port the port number on which to listen.
+     */
+    public Serve(int port) {
+        super(port);
+    }
+
+    @Override
+    protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+
+    }
 }
