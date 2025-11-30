83c186f5d8e8db55d2e306af0008337c512254dd
andreibel <andreinw12@gmail.com>
2025-11-30 19:39:02 +0200

    convert JDBCConnector to singleton pattern

--- server/src/main/java/com/andreibel/server/dbController/JDBCConnector.java
+++ server/src/main/java/com/andreibel/server/dbController/JDBCConnector.java
@@ -1,4 +1,15 @@
 package com.andreibel.server.dbController;
 
+
+// singleton connector
 public class JDBCConnector {
+
+    private static JDBCConnector instance;
+    private JDBCConnector(){}
+    public static JDBCConnector getInstance(){
+        if(instance == null){
+            instance = new JDBCConnector();
+        }
+        return instance;
+    }
 }
