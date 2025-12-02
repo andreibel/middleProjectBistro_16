56610e4f9dcf280e756050d2681e36a2ddc67f2f
AvivPeer <aviv.per@gmail.com>
2025-12-02 20:43:57 +0200

    Created OrderService , Message , Serve

--- server/src/main/java/com/andreibel/server/controller/Serve.java
+++ server/src/main/java/com/andreibel/server/controller/Serve.java
@@ -1,10 +1,16 @@
 package com.andreibel.server.controller;
 
+import com.andreibel.server.entity.Order;
+import com.andreibel.server.message.APICallType;
+import com.andreibel.server.message.Message;
+import com.andreibel.server.services.OrderService;
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
 
 public class Serve extends AbstractServer {
     public static final int PORT = 8080;
+    private OrderService orderService;
+
     /**
      * Constructs a new server.
      *
@@ -12,10 +18,53 @@ public class Serve extends AbstractServer {
      */
     public Serve(int port) {
         super(port);
+        this.orderService = OrderService.getInstance();
     }
 
     @Override
     protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+        try {
+            if (!(msg instanceof Message)) {
+                client.sendToClient("Invalid message format");
+                return;
+            }
 
+            Message message = (Message) msg;
+            APICallType callType = message.getType();
+            Object response = null;
+
+            switch (callType) {
+                case GET_ORDERS:
+                    response = orderService.getAllOrders();
+                    break;
+
+                case UPDATE_ORDER:
+                    Order order = (Order) message.getData();
+                    orderService.updateOrder(order);
+                    response = "Order updated successfully";
+                    break;
+
+                default:
+                    response = "Unknown request type";
+            }
+
+            client.sendToClient(response);
+
+        } catch (ClassCastException e) {
+            System.err.println("Error casting message data: " + e.getMessage());
+            try {
+            	client.sendToClient("Error: Invalid data format");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception e) {
+            System.err.println("Error processing request: " + e.getMessage());
+            e.printStackTrace();
+            try {
+            	client.sendToClient("Server error: " + e.getMessage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
--- server/src/main/java/com/andreibel/server/services/OrderService.java
+++ server/src/main/java/com/andreibel/server/services/OrderService.java
@@ -1,4 +1,33 @@
 package com.andreibel.server.services;
 
+import com.andreibel.server.dbController.repository.OrderRepository;
+import com.andreibel.server.entity.Order;
+
+import java.util.List;
+
 public class OrderService {
+    private static OrderService instance;
+    private final OrderRepository orderRepository;
+
+    private OrderService() {
+        this.orderRepository = OrderRepository.getInstance();
+    }
+
+    public static OrderService getInstance() {
+        if (instance == null) {
+            instance = new OrderService();
+        }
+        return instance;
+    }
+
+    public List<OrderResponse> getAllOrders() {
+    	List<Order> orders = orderRepository.getAllOrders();
+        return orders.stream().map((Order order)->new
+        		OrderResponse(order.getOrderNumber()
+        				,order.))
+    }
+
+    public void updateOrder(Order order) {
+        orderRepository.editOrder(order);
+    }
 }
