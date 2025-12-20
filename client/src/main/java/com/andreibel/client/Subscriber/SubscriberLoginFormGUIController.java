547fbf331899e8e0f3c031f01b8e92e65f66c061
Asafe51 <asafeli9812@gmail.com>
2025-12-20 20:21:43 +0200

    Finished the Table and Subscribers Forms Designs, only Worker remains

--- client/src/main/java/com/andreibel/client/Subscriber/SubscriberLoginFormGUIController.java
+++ client/src/main/java/com/andreibel/client/Subscriber/SubscriberLoginFormGUIController.java
@@ -1,4 +1,21 @@
 package com.andreibel.client.Subscriber;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class SubscriberLoginFormGUIController {
+
+    @FXML
+    private TextField txtFieldSubscriberId;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnGoBack;
+
+    public void onLoginButtonClicked(ActionEvent event) {}
+
+    public void onGoBackButtonClicked(ActionEvent event) {}
+
 }
--- client/src/main/java/com/andreibel/client/Table/LeaveTableFormGUIController.java
+++ client/src/main/java/com/andreibel/client/Table/LeaveTableFormGUIController.java
@@ -1,4 +1,20 @@
 package com.andreibel.client.Table;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class LeaveTableFormGUIController {
+    @FXML
+    private Label lblTitle;
+    @FXML
+    private Button btnPay;
+
+    @FXML
+    public void initialize() {}
+
+    public void onButtonPayClicked(ActionEvent event) {}
 }
+
+//Message if 2 hours has passed: Your time is up! Thank you for dining at Bistro Restaurant!
