49284f4c4b53bfcb2ea5b88a5c2c56c678b1fe65
ShayGonen1 <shgonen08@gmail.com>
2025-12-20 20:53:45 +0200

    added event handlers of MainGUIController and "go back" button event

--- client/src/main/java/com/andreibel/client/Main/MainFormGUIController.java
+++ client/src/main/java/com/andreibel/client/Main/MainFormGUIController.java
@@ -4,10 +4,17 @@ import com.andreibel.client.Client.BistroClientController;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormGUIController {
 
@@ -34,20 +41,81 @@ public class MainFormGUIController {
     public void setController(BistroClientController controller) {
         this.controller = controller;
     }
+    @FXML
+    public void onOrderButtonClicked(ActionEvent event) throws IOException {
+        // Load the new FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Order/OrderForm.fxml"));
+        Parent root = (Parent) loader.load();
 
-    public void onOrderButtonClicked(ActionEvent event) {
+        // Get current stage (window) from the event source
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set new scene
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("OrderFromGUIController");
+        stage.show();
 
     }
-    public void onArrivedButtonClicked(ActionEvent event) {
+
+    public void onArrivedButtonClicked(ActionEvent event) throws IOException{
+        // Load the new FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Table/GetTableForm.fxml"));
+        Parent root = (Parent) loader.load();
+        // Get current stage (window) from the event source
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set new scene
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("GetTableForm");
+        stage.show();
 
     }
-    public void onCancelButtonClicked(ActionEvent event) {
+    @FXML
+    public void onCancelButtonClicked(ActionEvent event) throws IOException{
+        // Load the new FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Order/CancelOrderForm.fxml"));
+        Parent root = (Parent) loader.load();
+
+        // Get current stage (window) from the event source
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set new scene
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("CancelOrderForm");
+        stage.show();
 
     }
-    public void onSubscriberButtonClicked(ActionEvent event) {
+    public void onSubscriberButtonClicked(ActionEvent event) throws IOException{
+        // Load the new FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Subscriber/SubscriberLoginForm.fxml"));
+        Parent root = (Parent) loader.load();
+
+        // Get current stage (window) from the event source
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set new scene
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("SubscriberLoginForm");
+        stage.show();
 
     }
-    public void onWorkerButtonClicked(ActionEvent event) {
+    public void onWorkerButtonClicked(ActionEvent event) throws IOException{
+        // Load the new FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Worker/WorkerLoginForm.fxml"));
+        Parent root = (Parent) loader.load();
+
+        // Get current stage (window) from the event source
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set new scene
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("WorkerLoginForm");
+        stage.show();
 
     }
 
