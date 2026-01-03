5752527b910036a125b3f08743e52758b99a7556
andreibel <andreinw12@gmail.com>
2026-01-03 19:54:49 +0200

    Add new DTOs: `BistroTimeRequest` and `SpecialDayRequest` for time and schedule handling.

---
+++ message/src/main/java/com/andreibel/message/DTO/BistroTimeRequest.java
@@ -0,0 +1,9 @@
+package com.andreibel.message.DTO;
+
+import java.time.LocalTime;
+
+public class BistroTimeRequest {
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Integer interval;
+}
